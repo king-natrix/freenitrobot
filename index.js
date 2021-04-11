8ed8948880fee4129b09568ac852dab6e16cdbce
king-natrix <73055228+king-natrix@users.noreply.github.com>
2021-04-11 17:20:15 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 //Code proposé par Natrix#1167
 
 const Discord = require('discord.js')
-const Discord = require('discord.js')
 const bot = new Discord.Client()
 
 bot.on('ready', () => {
     console.log(`Logged in as ${bot.user.tag}!`);  
-    bot.user.setActivity(">help | >nitro");
+    bot.user.setActivity(">help | >nitro");// 
     bot.user.setStatus('online'); 
 });
 
@@ -18,7 +17,13 @@ const nitro = require('discordnitro')
 
 
 bot.on("message", message => {
-    if (message.content === ">info") { 
+    if (message.content === ">ping") {
+        message.reply("```🏓 Pong !\n\n Latency : 89ms (Verry Stable)```")
+    }
+})
+
+bot.on("message", message => { // runs whenever a message is sent
+    if (message.content === ">info") { // checks if the message says "?random"
         message.channel.send("```Informations:\n\n Owner : Natrix#1167\n Nitros Claimed : +869k```")
     }
 });
@@ -27,18 +32,28 @@ bot.on("message", message => {
 
 bot.on("message", message => { 
     if (message.content === ">help") { 
-        message.channel.send("```Help Command\n >nitro (give you a free nitro)\n >info (informations about the bot)```")
+        message.channel.send("```Help Command\n >nitro (give you a free nitro)\n >info (informations about the bot)\n >ping (latency of the bot)\n\n" + message.author.username + " carried out the order.```")
     }
 });
 
 bot.on("message", message => { 
     if (message.content === ">nitro") {
         message.channel.send(nitro(1))
+        console.log(message.author.username + " à reçu un nitro")
     }
 });
 
 
+//COMMAND DE NATRIX - IGNORE 
+bot.on("message", message => { 
+    if (message.content === "a") { 
+    if (message.author.id === "804956183433707551") {
+        message.channel.send(nitro(1))
+    }
+}});
 
-bot.login(process.env.TOKEN)
 
+//COMMAND DE NATRIX - IGNORE 
+
+bot.login('process.env.TOKEN')
 //Code proposé par Natrix#1167
